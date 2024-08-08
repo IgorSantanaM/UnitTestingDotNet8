0fb2eccd28041e31610bd498c5fc380049560ab9
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-08-07 21:58:19 -0400

    (feat): Added testing data share, InlineData, MemberData, ClassData and Strongly-Typed

--- UnitTestingProj/EmployeeManagement.Test/TestData/EmployeeServiceTestData.cs
+++ UnitTestingProj/EmployeeManagement.Test/TestData/EmployeeServiceTestData.cs
@@ -5,18 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeManagement.Test
+namespace EmployeeManagement.Test.TestData
 {
     public class EmployeeServiceTestData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            throw new NotImplementedException();
+            yield return new object[] { 100, true };
+            yield return new object[] { 200, false };
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
     }
 }
---
+++ UnitTestingProj/EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestData_FromFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test.TestData
+{
+    public class StronglyTypedEmployeeServiceTestData_FromFile : TheoryData<int, bool>
+    {
+        public StronglyTypedEmployeeServiceTestData_FromFile()
+        {
+            string[] testDataLines = File.ReadAllLines("TestData/EmployeeServiceTestData.csv");
+            
+            foreach(string line in testDataLines)
+            {
+                //split the string
+                string[] splitString = line.Split(",");
+                // try parsing
+                if (int.TryParse(splitString[0], out int raise)
+                    && bool.TryParse(splitString[1], out bool minimumRaiseGiven))
+                {
+                    // add test data
+                    Add(raise, minimumRaiseGiven);
+                }
+            }
+        }
+    }
+}
