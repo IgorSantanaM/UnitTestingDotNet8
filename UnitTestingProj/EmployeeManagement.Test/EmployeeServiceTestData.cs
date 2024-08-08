b0168249c9aed9ec63010eb8dcfec3d9cb936e50
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-08-07 21:23:30 -0400

    (feat): Addeded theory using inline and memberdata

---
+++ UnitTestingProj/EmployeeManagement.Test/EmployeeServiceTestData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeServiceTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
