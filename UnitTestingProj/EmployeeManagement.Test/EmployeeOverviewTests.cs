4efcaec21a81c6459a60f4f9b55edaeb890210f1
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-08-12 15:54:20 -0400

    (feat): Added HttpContext.User Role Claims tests

--- UnitTestingProj/EmployeeManagement.Test/EmployeeOverviewTests.cs
+++ UnitTestingProj/EmployeeManagement.Test/EmployeeOverviewTests.cs
@@ -3,25 +3,23 @@ using EmployeeManagement.Business;
 using EmployeeManagement.Controllers;
 using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Claims;
 using Xunit;
 
 namespace EmployeeManagement.Test
 {
-    public class EmployeeOverviewControllerTests
+    public class EmployeeOverviewTests
     {
         private readonly EmployeeOverviewController _employeeOverviewController;
         private readonly InternalEmployee _firstEmployee;
-        public EmployeeOverviewControllerTests()
+
+        public EmployeeOverviewTests()
         {
-            _firstEmployee = new InternalEmployee("Megan", "Jones", 2, 3000, false, 2)
+            _firstEmployee = new InternalEmployee(
+                "Megan", "Jones", 2, 3000, false, 2)
             {
                 Id = Guid.Parse("bfdd0acd-d314-48d5-a7ad-0e94dfdd9155"),
                 SuggestedBonus = 400
@@ -32,40 +30,29 @@ namespace EmployeeManagement.Test
                 .Setup(m => m.FetchInternalEmployeesAsync())
                 .ReturnsAsync(new List<InternalEmployee>() {
                     _firstEmployee,
-                    new InternalEmployee("Jaimy", "Johnson", 3, 3400, true, 1)
-                    {
-                        Id = Guid.Parse("7183748a-ebeb-4355-8084-f190f8a5a68f"),
-                        SuggestedBonus = 500
-                    },
+                    new InternalEmployee("Jaimy", "Johnson", 3, 3400, true, 1),
                     new InternalEmployee("Anne", "Adams", 3, 4000, false, 3)
-                    {
-                        Id = Guid.Parse("e2bba7cf-dca7-433d-9ba6-79c834a02c48"),
-                        SuggestedBonus = 600
-                    }
                 });
 
             //var mapperMock = new Mock<IMapper>();
             //mapperMock.Setup(m =>
-            //    m.Map<InternalEmployee, ViewModels.InternalEmployeeForOverviewViewModel>
-            //    (It.IsAny<InternalEmployee>()))
-            //    .Returns(new ViewModels.InternalEmployeeForOverviewViewModel());
-
-            //_employeeOverviewController = new EmployeeOverviewController(
-            //   employeeServiceMock.Object, mapperMock.Object);
+            //    m.Map<InternalEmployee, 
+            //        ViewModels.InternalEmployeeForOverviewViewModel>
+            //        (It.IsAny<InternalEmployee>()))
+            //    .   Returns(new ViewModels.InternalEmployeeForOverviewViewModel());
 
             var mapperConfiguration = new MapperConfiguration(
-                cfg => cfg.AddProfile<MapperProfiles.EmployeeProfile>());
+               cfg => cfg.AddProfile<MapperProfiles.EmployeeProfile>());
             var mapper = new Mapper(mapperConfiguration);
 
             _employeeOverviewController = new EmployeeOverviewController(
-                employeeServiceMock.Object, mapper);
+               employeeServiceMock.Object, mapper);
         }
-
 
         [Fact]
         public async Task Index_GetAction_MustReturnViewResult()
         {
-            // Arrange 
+            // Arrange
 
             // Act
             var result = await _employeeOverviewController.Index();
@@ -75,7 +62,7 @@ namespace EmployeeManagement.Test
         }
 
         [Fact]
-        public async Task Index_GetAction_MustReturnEmployeeOverviewViewModelAsModelType()
+        public async Task Index_GetAction_MustReturnEmployeeOverviewViewModelAsViewModelType()
         {
             // Arrange
 
@@ -83,8 +70,11 @@ namespace EmployeeManagement.Test
             var result = await _employeeOverviewController.Index();
 
             // Assert
-            Assert.IsAssignableFrom<EmployeeOverviewViewModel>(
-                ((ViewResult)result).Model);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<EmployeeOverviewViewModel>(viewResult.Model);
+
+            //Assert.IsType<EmployeeOverviewViewModel>(
+            //  ((ViewResult)result).Model);
         }
 
         [Fact]
@@ -95,8 +85,9 @@ namespace EmployeeManagement.Test
             // Act
             var result = await _employeeOverviewController.Index();
 
-            // Assert 
-            Assert.Equal(3, ((EmployeeOverviewViewModel)((ViewResult)result).Model)
+            // Assert
+            Assert.Equal(3,
+                ((EmployeeOverviewViewModel)((ViewResult)result).Model)
                 .InternalEmployees.Count);
         }
 
@@ -110,7 +101,7 @@ namespace EmployeeManagement.Test
 
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result);
-            var viewModel = Assert.IsAssignableFrom<EmployeeOverviewViewModel>(viewResult.Model);
+            var viewModel = Assert.IsType<EmployeeOverviewViewModel>(viewResult.Model);
             Assert.Equal(3, viewModel.InternalEmployees.Count);
             var firstEmployee = viewModel.InternalEmployees[0];
             Assert.Equal(_firstEmployee.Id, firstEmployee.Id);
@@ -119,6 +110,66 @@ namespace EmployeeManagement.Test
             Assert.Equal(_firstEmployee.Salary, firstEmployee.Salary);
             Assert.Equal(_firstEmployee.SuggestedBonus, firstEmployee.SuggestedBonus);
             Assert.Equal(_firstEmployee.YearsInService, firstEmployee.YearsInService);
+        }
+
+        [Fact]
+        public void ProtectedIndex_GetActionForUserInAdminRole_MustRedirectToAdminIndex()
+        {
+            // Arrange
+            var userClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Karen"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+            var claimsIdentity = new ClaimsIdentity(userClaims, "UnitTest");
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var httpContext = new DefaultHttpContext()
+            {
+                User = claimsPrincipal
+            };
+
+            _employeeOverviewController.ControllerContext =
+                new ControllerContext()
+                {
+                    HttpContext = httpContext
+                };
+
+            // Act
+            var result = _employeeOverviewController.ProtectedIndex();
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("AdminIndex", redirectToActionResult.ActionName);
+            Assert.Equal("EmployeeManagement", redirectToActionResult.ControllerName);
+        }
+
+        [Fact]
+        public void ProtectedIndex_GetActionForUserInAdminRole_MustRedirectToAdminIndex_WithMoq()
+        {
+            // Arrange
+            var mockPrincipal = new Mock<ClaimsPrincipal>();
+            mockPrincipal.Setup(x => x.IsInRole(It.Is<string>(s => s == "Admin")))
+                .Returns(true);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.User)
+                .Returns(mockPrincipal.Object);
+
+
+            _employeeOverviewController.ControllerContext =
+                new ControllerContext()
+                {
+                    HttpContext = httpContextMock.Object
+                };
+
+            // Act
+            var result = _employeeOverviewController.ProtectedIndex();
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("AdminIndex", redirectToActionResult.ActionName);
+            Assert.Equal("EmployeeManagement", redirectToActionResult.ControllerName);
         }
     }
 }
